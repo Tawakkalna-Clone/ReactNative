fa7f26a2a1b220cf9d60fdd929c68da495266544
hassanalgadah-tuwaiq <Hassan.algadah@tuwaiq.edu.sa>
2021-07-30 02:04:59 +0300

    add: first half of page one

--- Tawakkalna-Clone/App.js
+++ Tawakkalna-Clone/App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, Image, ImageBackground, View} from 'react-native';
+
+var {bp, vw, vh} = require('react-native-relative-units')(375);
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    }
+})
