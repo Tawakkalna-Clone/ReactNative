174f04f35160cdb24c3ea69e29df8f97ecf79cbf
Mutaz Alajlan <Mutaz.Alajlan@tuwaiq.edu.sa>
2021-07-29 18:30:09 +0300

    Feat: Services screen

--- Tawakkalna-Clone/screens/Services.js
+++ Tawakkalna-Clone/screens/Services.js
@@ -1,31 +1,47 @@
 import React from 'react'
-import { View, Text, I18nManager } from 'react-native'
+import { View, Text, I18nManager, ScrollView } from 'react-native'
 
 export default function Services() {
 
     return (
-        <View style={{ flex: 10 }}>
 
-            <View style={{ flex: 2, marginTop: 100, marginLeft: 30 }}>
-                <Text style={{ color: "white", fontSize: 22, textAlign: 'left' }}>
-                    الخدمات العامة</Text>
-                <View style={{ flexWrap: "wrap", flexDirection: "row" }}>
-
-                    <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+        <View style={{ flex: 1, backgroundColor: "#1c1c1c" }}>
+            <ScrollView>
+                <View style={{ flex: 1, marginTop: 80, marginLeft: 30 }}>
+                    <Text style={{ color: "white", fontSize: 22, textAlign: 'left', marginBottom: 5 }}>
+                        الخدمات العامة</Text>
+                    <View style={{ flexWrap: "wrap", flexDirection: "row" }}>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
                     </View>
                 </View>
 
-            </View>
-
-            <View style={{ flex: 2, marginLeft: 30 }}>
-                <Text style={{ color: "white", textAlign: "left", fontSize: 22 }}>
-                    الخدمات العامة</Text>
-                <View style={{ flexWrap: "wrap", flexDirection: "row" }}>
-                    <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                <View style={{ flex: 2, marginLeft: 30 }}>
+                    <Text style={{ color: "white", textAlign: "left", fontSize: 22, marginBottom: 5, marginTop: 25 }}>
+                        خدمات التصاريح  </Text>
+                    <View style={{ flexWrap: "wrap", flexDirection: "row", justifyContent: "space-between" }}>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
                     </View>
                 </View>
 
-            </View>
+                <View style={{ flex: 1, marginLeft: 30 }}>
+                    <Text style={{ color: "white", textAlign: "left", fontSize: 22, marginBottom: 5, marginTop: 25 }}>
+                        خدمات الصحة  </Text>
+                    <View style={{ flexWrap: "wrap", flexDirection: "row", justifyContent: "space-between" }}>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
+                        <View style={{ height: 150, width: 150, backgroundColor: "white" }}>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
