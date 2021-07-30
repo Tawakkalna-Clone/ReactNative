897b99461b7e88d6cbcda029dc324374ca64518b
Mohammed <Mohammed@example.com>
2021-07-30 17:23:03 +0300

    integerate profile and status cards

---
+++ Tawakkalna-Clone/components/ProfileCard.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {StyleSheet, Text, Image, ImageBackground, View} from 'react-native';
+
+let {bp, vw, vh} = require('react-native-relative-units')(375);
+
+
+export default function ProfileCard() {
+    return (
+        <View >
+            <ImageBackground source={require('../Assets/Background.jpeg')} imageStyle={{borderRadius: 15}}
+                             resizeMode="cover" style={styles.cont}>
+                <View style={styles.profileimgcont}>
+                    <View style={styles.outerCircle}>
+                        <View style={styles.innerCircle}>
+                            <Image style={styles.profileImage} source={require('../Assets/download.png')}/>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.textcont}>
+                    <Text style={styles.tex}>حسن القعده</Text>
+                </View>
+                <View style={styles.bottomimages}>
+                    <View style={styles.greenthing}/>
+                    <Image style={styles.tokalnalogo} source={require('../Assets/logocard.png')}/>
+                </View>
+            </ImageBackground>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    cont: {
+        width: 72 * vw,
+        height: 25 * vh,
+    },
+    textcont: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    tex: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#2fa595'
+    },
+    profileimgcont: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    outerCircle: {
+        borderRadius: 60,
+        width: 80,
+        height: 80,
+        backgroundColor: '#2fa595',
+        marginTop: 2,
+    },
+    innerCircle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 60,
+        width: 75,
+        height: 75,
+        margin: 2.5,
+        backgroundColor: '#cbcbcb'
+    },
+    bottomimages: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    tokalnalogo: {
+        resizeMode: 'contain',
+        width: 20 * vw,
+        height: 13 * vh,
+        marginRight: 10
+    },
+    Image: {
+        resizeMode: 'contain'
+    },
+    greenthing: {
+        backgroundColor: '#005d26',
+        width: 22 * vw,
+        height: 3 * vh,
+        borderTopRightRadius: 35,
+        borderBottomRightRadius: 35
+    },
+    profileImage: {
+        resizeMode: 'contain',
+        width: 13 * vw,
+        height: 14 * vh,
+    }
+})
--- Tawakkalna-Clone/screens/Home.js
+++ Tawakkalna-Clone/screens/Home.js
@@ -1,21 +1,27 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import ProfileCard from '../components/ProfileCard';
+import StatusCard from '../components/StatusCard';
 
 export default function HomeScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ marginTop: 50, height: 50, width: '100%', backgroundColor: 'red', flexDirection: 'row-reverse' }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: "#0f1217"  }}>
+            <View style={{ marginTop: 50, height: 50, width: '100%', flexDirection: 'row-reverse' }}>
                 <View style={{ marginRight: 50, height: 50, width: 50, backgroundColor: 'blue' }}>
-                    <Text>jrs22</Text>
-
                 </View>
             </View>
-            <View style={{ flex: 3, marginTop: 20, backgroundColor: 'red', width: '100%' }}>
-                <Text>jrs</Text>
-            </View>
-            <View style={{ flex: 2, marginTop: 20, backgroundColor: 'yellow', width: '100%' }}>
-                <Text>jrs</Text>
-            </View>
+            <ProfileCard/> 
+
+            {/* <View style={{ flex: 3, marginTop: 20, backgroundColor: 'red', width: '90%' }}>
+                <View style ={{}}>
+
+                </View>
+
+            </View> */}
+
+            <StatusCard/>
+            {/* <View style={{ flex: 2, marginTop: 20, backgroundColor: 'yellow', width: '100%' }}>
+            </View> */}
             <View style={{ flex: 2, marginTop: 20, backgroundColor: 'yellow', width: '100%' }}>
                 <Text>jrs</Text>
             </View>
