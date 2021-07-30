6e0f1676ef70e46888559e3b3dec7b13c968bdc1
Mutaz Alajlan <Mutaz.Alajlan@tuwaiq.edu.sa>
2021-07-30 20:51:36 +0300

    Add: logout button

---
+++ Tawakkalna-Clone/components/SignOut.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+export default function SignOut() {
+    return (
+        <View>
+            <View style={{
+                flex: 0.5, justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <TouchableOpacity style={styles.button}><Text style={styles.text} onPress={() => navigation.navigate("SignUp")}>تسجيل الخروج</Text></TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 55,
+        width: '95%',
+        borderRadius: 7,
+        backgroundColor: '#c73f3f',
+    },
+    text: {
+        color: 'white',
+        fontSize: 20,
+    }
+});
--- Tawakkalna-Clone/screens/MyAccount.js
+++ Tawakkalna-Clone/screens/MyAccount.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View, ScrollView, Image } from 'react-native';
 import AccountSetting from '../components/AccountSetting';
 import About from '../components/About';
+import SignOut from '../components/SignOut';
 export default function MyAccountScreen() {
 
     return (
@@ -20,10 +21,13 @@ export default function MyAccountScreen() {
                         حول التطبيق</Text>
                     <About />
                 </View>
-                <View style={{ flex: 1, alignItems: "center", marginTop: -40 }}>
+                <View style={{ flex: 1, alignItems: "center", marginTop: -30 }}>
                     <Image style={{ resizeMode: 'contain', width: "80%" }} source={require('../Assets/Logo-2.png')} />
                     <Text style={{ color: "#707378", fontSize: 25, textAlign: 'left', marginTop: -30 }}>
-                        V 3.0.1</Text>
+                        V 3.0.5</Text>
+                </View>
+                <View style={{ flex: 1, margin: 10 }}>
+                    <SignOut />
                 </View>
             </ScrollView>
         </View>
